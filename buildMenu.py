227227d95bfd07aa7d55f6ebd7ab2f06885cf1c8
James <squaffle@hotmail.com>
2016-04-23 18:43:02 +0100

    can now move between scenes

--- buildMenu.py
+++ buildMenu.py
@@ -17,11 +17,11 @@ import cocos
 class BuildSelectScene(cocos.layer.ColorLayer):
 
     def __init__(self):
-        super(BuildSelectScene, self).__init__(64,64,224,255) #constructor
+        super(BuildSelectScene, self).__init__(230,149,18,255) #constructor
 
         # a cocos.text.Label is a wrapper of pyglet.text.Label
         # with the benefit of being a cocosnode
-        label = cocos.text.Label('Hello, World!',
+        label = cocos.text.Label('Scene 1',
                                  font_name='Times New Roman',
                                  font_size=32,
                                  anchor_x='center', anchor_y='center')
@@ -29,20 +29,22 @@ class BuildSelectScene(cocos.layer.ColorLayer):
         label.position = 320, 240
         self.add(label)
 
-    CCMenuItem = cocos.menu.MenuItem   # itemFromNormalImage:@"ButtonStar.png" selectedImage:@"ButtonStarSel.png" 
-    c
-  target:self selector:@selector(starButtonTapped:)]
-    starMenuItem.position = ccp(60, 60)
-    CCMenu *starMenu = [CCMenu menuWithItems:starMenuItem, nil]
-    starMenu.position = CGPointZero;
-    [self addChild:starMenu]
+class BuildMenu(cocos.menu.Menu):
+    def __init__(self):
+        super(BuildMenu, self).__init__("Click the apple")
+
+        CCMenuItem = cocos.menu.ImageMenuItem("ico-res-fd.png", self.onButtonClick)
+        self.create_menu([CCMenuItem])
+
+    def onButtonClick(self):
+        cocos.director.director.run(buildingInfo_scene)
 
 class BuildInfoScene(cocos.layer.ColorLayer):
 
     def __init__(self):
         super(BuildInfoScene, self).__init__(64,64,224,255) #constructor
 
-        label = cocos.text.Label('Hello, World!',
+        label = cocos.text.Label('Scene 2',
                                  font_name='Times New Roman',
                                  font_size=32,
                                  anchor_x='center', anchor_y='center')
@@ -54,13 +56,10 @@ if __name__ == "__main__":
     # director init takes the same arguments as pyglet.window
     cocos.director.director.init()
 
-    # Create the Layers ready to create Scenes from
-    buildSelect_layer = BuildSelectScene()
-    buildInfo_layer = BuildInfoScene()
-
     # Create the scenes
-    buildingSelect_scene = cocos.scene.Scene(buildSelect_layer)
-    buildingInfo_scene = cocos.scene.Scene(buildInfo_layer)
+    buildingSelect_scene = cocos.scene.Scene(BuildSelectScene())
+    buildingSelect_scene.add(BuildMenu())
+    buildingInfo_scene = cocos.scene.Scene(BuildInfoScene())
 
     # And now, start the application, starting with main_scene
     cocos.director.director.run(buildingSelect_scene)
