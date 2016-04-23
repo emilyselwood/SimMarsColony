a85ae666eed191ad2aa2e9afb0fe7377cbe9bc5d
Louise Dennis <louise@dennis-sellers.com>
2016-04-23 15:41:33 +0100

    Initial Game Data class

---
+++ GameData.py
@@ -0,0 +1,10 @@
+class GameData(object):
+
+    def __init__(self):
+        self.habitatmaterial = 0
+
+    def setHabitatMaterial(self, n):
+        self.habitatmaterial = n
+
+    def getHabitatMaterial(self):
+        return self.habitatmaterial
