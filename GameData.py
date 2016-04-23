e1a461f9877e0a307da86284cd675b8cacc739d3
Louise Dennis <louise@dennis-sellers.com>
2016-04-23 16:18:54 +0100

    Basic text only version

--- GameData.py
+++ GameData.py
@@ -2,9 +2,26 @@ class GameData(object):
 
     def __init__(self):
         self.habitatmaterial = 0
+        self.habitats = 0
+        self.habitatcost = 2
+        self.habitatproductivity = 2
 
     def setHabitatMaterial(self, n):
         self.habitatmaterial = n
 
+    def addHabitatMaterial(self, n):
+        self.habitatmaterial = self.habitatmaterial + int(n)
+
     def getHabitatMaterial(self):
         return self.habitatmaterial
+
+    def getHabitats(self):
+        return self.habitats
+
+    def makeHabitats(self, number):
+        n = int(number)
+        self.habitatmaterial = self.habitatmaterial - n*self.habitatcost
+        self.habitats = self.habitats + n
+
+    def produce(self):
+        self.habitatalmaterial = self.habitatmaterial + self.habitats*self.habitatproductivity
